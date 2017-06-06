c134f79f910ba75158fa2a6d84fc7d7e9626dff9
RaynieSun <huangyaqin2016@gmail.com>
2017-06-06 15:29:56 +0800

    fixbug

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['env', 'react']，
+              presets: ['env', 'react'],
               plugins: ['transform-runtime']
             }
           }
